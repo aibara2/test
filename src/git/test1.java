a04c1357484a3a68cf52fbd0fffab506bd1af5ca
aibara <aibara@cmedia.co.jp>
2014-10-02 17:47:11 +0900

    commit5

--- src/git/test1.java
+++ src/git/test1.java
@@ -7,7 +7,7 @@ public class test1 {
 	static String str="あいうえお";
 	static String str2="かきくけこ";
 	static int a=0;
-	static int b=1;
+	// static int b=1;
 
 	public static  void main(){
 		System.out.println(str+a);
